179967a567603db1c7c80bd5cc712d1177e20d42
aracely.955129 <aracely.955129@gmail.com>
2026-06-28 21:21:57 -0400

    fix render deployment structure

--- config.py
+++ config.py
@@ -7,7 +7,7 @@ class Config:
 
     BASE_DIR = os.path.abspath(os.path.dirname(__file__))
 
-    SQLALCHEMY_DATABASE_URI = "sqlite:///" + os.path.join(BASE_DIR, "instance", "safecity.db")
+    SQLALCHEMY_DATABASE_URI = "sqlite:///" + os.path.join(BASE_DIR, "app", "instance", "safecity.db")
 
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
--- run.py
+++ run.py
@@ -1,5 +1,6 @@
 from app import create_app
-
+import os
+os.makedirs("app/instance", exist_ok=True)
 app = create_app()
 
 if __name__ == "__main__":
